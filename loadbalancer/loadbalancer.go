943870a04e6388260e9313833b89e9a68bedd644
andrew brown <aebrownz11@gmail.com>
2016-08-16 15:58:35 -0700

    Refacted health strategy by moving utility functions to util file

--- loadbalancer/loadbalancer.go
+++ loadbalancer/loadbalancer.go
@@ -4,20 +4,12 @@ import (
   "net/http"
   "net/http/httputil"
   "net/url"
-  "log"
+  // "log"
   "reflect"
   "runtime"
-  "time"
-  "strings"
-  "encoding/json"
   "github.com/aebrow4/unloadx-lb/util"
 )
 
-// TODO
-// implement the other strategy functions
-
-// define a type that all strategy functions will implement
-// type strategy func([]*url.URL, []lbutil.ServerHealth) *httputil.ReverseProxy
 type strategy func([]*url.URL, []*lbutil.ServerHealth) *httputil.ReverseProxy
 
 func RoundRobin(servers []*url.URL, _ []*lbutil.ServerHealth) *httputil.ReverseProxy {
@@ -58,64 +50,11 @@ func Health(servers []*url.URL, healths []*lbutil.ServerHealth) *httputil.Revers
 func LoadBalance(fn strategy, servers[]*url.URL) {
   serverHealths := make([]*lbutil.ServerHealth, 0)
   var serverHealthsPtrs []*lbutil.ServerHealth
+
   // if strategy is health, poll servers for their health
   if runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name() == "github.com/aebrow4/unloadx-lb/loadbalancer.Health" {
-    // initialize array of health structs to store health info in, and store the
-    // port the user's process runs on since we will need to ping it later to check
-    // for availability
-    var serverPorts []string
-
-    for _, server := range servers {
-      currServerHealth := &lbutil.ServerHealth{
-        Address: strings.Split(server.Host, ":")[0],
-        Cpu: 0,
-        Mem: 0,
-      }
-      serverHealths = append(serverHealths, currServerHealth)
-      serverPorts = append(serverPorts, strings.Split(server.Host, ":")[1])
-    }
-
-    for _, val := range serverHealths {
-      serverHealthsPtrs = append(serverHealthsPtrs, val)
-    }
-
-    // send an HTTP request for each server in serverHealths
-    // updating the serverHealths structs with the response info
-    ticker := time.NewTicker(5 * time.Second)
-    quit := make(chan struct{})
-    go func() {
-        for {
-           select {
-            case <- ticker.C:
-              for i, serverHealth := range serverHealths[0:] {
-                r, _ := http.Get("http://" + serverHealth.Address + ":5000")
-                var jsonBody map[string]interface{}
-                dec := json.NewDecoder(r.Body)
-                dec.Decode(&jsonBody)
-                serverHealth.Cpu = jsonBody["cpu"].(float64)
-                serverHealth.Mem = jsonBody["memory"].(float64)
-                serverHealth.Avail = true
-
-                // update server to unavailable if status code doesn't begin with 2
-                // send a request to the server rather than the health service, since
-                // the health service may remain up even if the server goes down
-                // this is arguably an expensive way of checking for server availability,
-                // but better than pings which assume that the administrator has the ping
-                // server turned on
-                resp, err := http.Get("http://" + serverHealth.Address + ":" + serverPorts[i])
-                if resp == nil || err != nil || resp.StatusCode < 200 || resp.StatusCode >= 300 {
-                  serverHealth.Avail = false
-                }
-                log.Println("cpu: ", serverHealth.Cpu, " and mem: ", serverHealth.Mem)
-              }
-            case <- quit:
-              ticker.Stop()
-              return
-            }
-        }
-     }()
+    serverHealthsPtrs = lbutil.GetHealth(servers, serverHealths[0:], serverHealthsPtrs[0:]);
   }
-
 
   proxy := fn(servers, serverHealthsPtrs)
   http.ListenAndServe(":9090", proxy)
--- util/util.go
+++ util/util.go
@@ -2,6 +2,11 @@ package lbutil
 
 import (
   "log"
+  "net/url"
+  "net/http"
+  "encoding/json"
+  "strings"
+  "time"
 )
 
 // data structure for storing server metrics
@@ -29,9 +34,6 @@ func ChooseOnHealth(healths []*ServerHealth) int {
   var lowestMemIdx int = 0
   var lowestCpuIdx int = 0
   h0 := *healths[0]
-  h1 := *healths[1]
-  log.Println("h0: ", h0)
-  log.Println("h1: ", h1)
   var lowestMem = float64(h0.Mem)
   var lowestCpu = float64(h0.Cpu)
   var cpuIdxBelowLimit []int
@@ -62,14 +64,11 @@ func ChooseOnHealth(healths []*ServerHealth) int {
       cpuIdxBelowLimit = append(cpuIdxBelowLimit, i)
     }
   }
-  log.Println("got here: ", availServerIdx)
 
   // if a server has lowest memory and CPU, choose it
   // else consider all servers with CPU < .7.  Choose based on lowest memory
   // else if all servers have CPU over .7, choose based on lowest memory
   isLowestAvail := contains(availServerIdx, lowestMemIdx)
-  log.Println("lowestmemidx: ", lowestMemIdx)
-  log.Println(isLowestAvail)
   if lowestMemIdx == lowestCpuIdx && isLowestAvail {
     log.Println("avail indices: ", availServerIdx)
     log.Println("choosing idx: ", lowestMemIdx)
@@ -84,7 +83,60 @@ func ChooseOnHealth(healths []*ServerHealth) int {
         }
     }
   }
-  log.Println("avail indices: ", availServerIdx)
-  log.Println("choosing idx: ", lowestMemIdx)
   return lowestMemIdx
+}
+
+func GetHealth(servers[]*url.URL, serverHealths[]*ServerHealth, serverHealthsPtrs[]*ServerHealth) []*ServerHealth {
+  var serverPorts []string
+
+  for _, server := range servers {
+    currServerHealth := &ServerHealth{
+      Address: strings.Split(server.Host, ":")[0],
+      Cpu: 0,
+      Mem: 0,
+    }
+    serverHealths = append(serverHealths, currServerHealth)
+    serverPorts = append(serverPorts, strings.Split(server.Host, ":")[1])
+  }
+
+  for _, val := range serverHealths {
+    serverHealthsPtrs = append(serverHealthsPtrs, val)
+  }
+
+  // send an HTTP request for each server in serverHealths
+  // updating the serverHealths structs with the response info
+  ticker := time.NewTicker(1 * time.Second)
+  quit := make(chan struct{})
+  go func() {
+      for {
+         select {
+          case <- ticker.C:
+            for i, serverHealth := range serverHealths[0:] {
+              r, _ := http.Get("http://" + serverHealth.Address + ":5000")
+              var jsonBody map[string]interface{}
+              dec := json.NewDecoder(r.Body)
+              dec.Decode(&jsonBody)
+              serverHealth.Cpu = jsonBody["cpu"].(float64)
+              serverHealth.Mem = jsonBody["memory"].(float64)
+              serverHealth.Avail = true
+
+              // update server to unavailable if status code doesn't begin with 2
+              // send a request to the server rather than the health service, since
+              // the health service may remain up even if the server goes down
+              // this is arguably an expensive way of checking for server availability,
+              // but better than pings which assume that the administrator has the ping
+              // server turned on
+              resp, err := http.Get("http://" + serverHealth.Address + ":" + serverPorts[i])
+              if resp == nil || err != nil || resp.StatusCode < 200 || resp.StatusCode >= 300 {
+                log.Println("error ocurred in get request")
+                serverHealth.Avail = false
+              }
+            }
+          case <- quit:
+            ticker.Stop()
+            return
+          }
+      }
+   }()
+  return serverHealthsPtrs[0:]
 }
